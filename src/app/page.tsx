a83f3b2d918fd3abce8da7e18baea87d67d7d299
Brayan Annear <annear2000@gmail.com>
2023-11-19 20:22:54 -0500

    tarjeta y títulos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ const Home = () => {
   const [filtroTab, setFiltroTab] = useState(1);
   const [periodo, setPeriodo] = useState("2020");
   const [trimestre, setTrimestre] = useState("");
+  console.log(filtroTab, "filtroTab", periodo, "periodo", trimestre, "trimestre")
 
   useEffect(() => {
     fetch("https://www.datos.gov.co/resource/4z5v-cr6b.json")
@@ -38,7 +39,7 @@ const Home = () => {
 
   const filtroTraficoMasAlto = traficoMasAlto(datos, periodo)
   const pruebaProveedoresMasAlto = traficoMasAltoPrueba(datos, periodo, trimestre)
-  // console.log(pruebaProveedoresMasAlto)
+  console.log(pruebaProveedoresMasAlto, "pruebaproveedormásalto")
   const mejorAnoProveedor = mejorAñoCadaProveedor(datos)
 
   const periodo2020 = () => {
@@ -66,31 +67,53 @@ const Home = () => {
     setPeriodo("2023")
     setTrimestre("")
   }
-
+  let max = ""
+  let maxTrafico = pruebaProveedoresMasAlto.map((e, i) => (
+    e.tr_fico
+  ))
+  let maxTraficoConvert = maxTrafico.reduce((max, valor) => (valor > max ? valor : max), -Infinity)
+  console.log(maxTraficoConvert, "este es le valor")
 
   return (
     <div className="">
-      <div className="flex justify-center gap-10">
-        <button onClick={periodo2020} className={filtroTab === 1 ? 'active' : ''}>
-          2020
-        </button>
-        <button onClick={periodo2021} className={filtroTab === 2 ? 'active' : ''}>
-          2021
-        </button>
-        <button onClick={periodo2022} className={filtroTab === 3 ? 'active' : ''}>
-          2022
-        </button>
-        <button onClick={periodo2023} className={filtroTab === 4 ? 'active' : ''}>
-          2023
-        </button>
+      <div className=''>
+        <div className='target-explication'>
+          <div>
+            <h3 className='tittle-target'>{trimestre !== '' ? "Año y trimestre" : "Año por provedor"}</h3>
+          </div>
+          <div className="content-target">
+            <p>En el año {periodo} la empresa {pruebaProveedoresMasAlto.map((e, i) => (
+              e.tr_fico === maxTraficoConvert ? e.proveedor : ""
+            ))} tiene un tráfico total de: {maxTraficoConvert} siendo el tráfico más alto del {trimestre ? "trimestre" : "año"}
+            </p>
+          </div>
+
+        </div>
       </div>
-      <div className="flex justify-center gap-10">
-        <button onClick={() => setTrimestre("1")}>Trimestre 1</button>
-        <button onClick={() => setTrimestre("2")}>Trimestre 2</button>
-        <button onClick={() => setTrimestre("3")}>Trimestre 3</button>
-        <button onClick={() => setTrimestre("4")}>Trimestre 4</button>
+      <div className="container-tabla">
+        <div className="flex justify-center gap-10">
+
+          <button onClick={periodo2020} className={filtroTab === 1 ? 'active' : ''}>
+            2020
+          </button>
+          <button onClick={periodo2021} className={filtroTab === 2 ? 'active' : ''}>
+            2021
+          </button>
+          <button onClick={periodo2022} className={filtroTab === 3 ? 'active' : ''}>
+            2022
+          </button>
+          <button onClick={periodo2023} className={filtroTab === 4 ? 'active' : ''}>
+            2023
+          </button>
+        </div>
+        <div className="flex justify-center gap-10">
+          <button onClick={() => setTrimestre("1")}>Trimestre 1</button>
+          <button onClick={() => setTrimestre("2")}>Trimestre 2</button>
+          <button onClick={() => setTrimestre("3")}>Trimestre 3</button>
+          <button onClick={() => setTrimestre("4")}>Trimestre 4</button>
+        </div>
+        <LineChart dataTraficoAlto={pruebaProveedoresMasAlto} />
       </div>
-      <LineChart dataTraficoAlto={pruebaProveedoresMasAlto} />
     </div>
   );
 }
